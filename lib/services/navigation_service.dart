98cc03b34fe9944a99d78ee09fdb5409e9d07aab
Gregory Geevarghese <greg@oneroof.ai>
2025-01-10 10:30:27 -0500

    Fix : Fixed Navigation Service default index

--- lib/services/navigation_service.dart
+++ lib/services/navigation_service.dart
@@ -6,7 +6,6 @@ import '../presentation/pages/navbar/folder_page.dart';
 import '../presentation/pages/navbar/home_page.dart';
 import '../presentation/pages/navbar/people_page.dart';
 
-
 class NavigationService {
   static final NavigationService _instance = NavigationService._internal();
 
@@ -16,29 +15,25 @@ class NavigationService {
 
   NavigationService._internal();
 
-  int _currentIndex = 0;
+  int _currentIndex = 1;
   int get currentIndex => _currentIndex;
 
   Future<void> navigateToPage(BuildContext context, int index) async {
-
     _currentIndex = index;
     Widget page;
 
     switch (index) {
       case 1:
-        page = const HomePage(token: "ddasnjk",);
+        page = const HomePage(token: "ddasnjk");
+        break;
+      case 2:
+        page = ChatPage(appName: "ChatGPT");
         break;
       case 3:
         page = const FolderPage();
         break;
-      case 2:
-        page = ChatPage(appName:"ChatGPT");
-        break;
-      case 5:
-        page = const PeoplePage();
-        break;
       case 4:
-        page = const CubePage();
+        page = const PeoplePage();
         break;
       default:
         return;
